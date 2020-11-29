ee6deb53f77b67704137d04e0b853005bd28ef91
Loquicom <29797812+Loquicom@users.noreply.github.com>
2020-11-29 17:29:52 +0100

    MaJ CLI

--- view.js
+++ view.js
@@ -7,3 +7,5 @@ console.log(path.join(__dirname, '../'));
 console.log(file.list(path.join(__dirname, '../')));
 console.log(path.join(__dirname, 'dist'));
 console.log(file.list(path.join(__dirname, 'dist')));
+console.log(path.join(__dirname, 'dist/squirrel-windows'));
+console.log(file.list(path.join(__dirname, 'dist/squirrel-windows')));
