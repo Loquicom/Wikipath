9fcb956dd272569c8f1865998de363c607b4c24f
Loquicom <29797812+Loquicom@users.noreply.github.com>
2020-12-03 09:20:39 +0100

    Fin page resultat

--- src/view/page/result/script.js
+++ src/view/page/result/script.js
@@ -1,2 +1,21 @@
+// Variables
 const result = storage.get('result');
-console.log(result);
+scope.server = {name: storage.get('server-name')};
+
+// Functions
+function generateResult(result) {
+    let html = '';
+    for(let res of result) {
+        html += '<tr>';
+        html += `<td>${res.pos}</td>`;
+        html += `<td>${res.pseudo}</td>`;
+        html += `<td><button class="nes-btn is-primary" data-pos="${res.pos}">${_('result.view')}</button></td>`;
+        html += '</tr>';
+    }
+    return html;
+}
+
+// DOM Ready
+$(() => {
+    $('#result-content').html(generateResult(result));
+});
